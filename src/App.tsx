d93b7b9184c0316a285755376acb7c996d8df817
Dominik Stötter <dominik.stoetter@yahoo.com>
2023-07-12 22:28:10 +0200

    add wnderlvst-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,14 @@
 import React, { useState } from 'react'
 import { invoke } from '@tauri-apps/api/tauri'
-
+import FileList from 'wnderlvst-react-components/dist/components/FileList'
 import './App.css'
-import { DndExample } from './Dnd'
 
 function App() {
-  const [msgFromRust, setMsgFromRust] = useState('')
-  const [inputValue, setInputValue] = useState('')
-  const [isDndView, setIsDndView] = useState(false)
-
-  const handleHelloWorld = async () => {
-    try {
-      const response = await invoke('hello_world_test', {
-        event: inputValue || 'nope',
-      })
-      setMsgFromRust(`${response}`)
-      console.log('response ', response)
-    } catch (error) {
-      console.log('error ', error)
-    }
-  }
-
   return (
     <div className='app'>
-      <div className='btn-toggle-container'>
-        <button
-          className='btn-toggle'
-          aria-label='toggle view'
-          onClick={() => setIsDndView(!isDndView)}
-        >{isDndView ? '💧' : '🔥'}</button>
-      </div>
-      {isDndView ? (
-        <DndExample />
-      ) : (
-        <header className='App-header'>
-          <div className='component-wrapper'>
-            <input
-              value={inputValue}
-              placeholder='input for rust'
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-            <button className='btn' onClick={handleHelloWorld}>
-              call rust
-            </button>
-            {!!msgFromRust && (
-              <p style={{ position: 'absolute' }}>
-                response message: {msgFromRust}
-              </p>
-            )}
-          </div>
-        </header>
-      )}
+      <FileList items={
+        [{name: "test", isDirectory: true, items: [{name: "test3"}]}, {name: "test2"}]
+      }/>
     </div>
   )
 }
